676e4071e5f67208f7ed4521c45ce03de252673a
TrentHurlbut <trent.hurlbut@gmail.com>
2022-03-17 06:26:15 -0700

    Added Login and Sign Up links.

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import CreateLink from './CreateLink';
 import Header from './Header';
 import LinkList from './LinkList';
+import Login from './Login';
 import { Route, Routes } from 'react-router-dom';
 
 const App = () => {
     return (
-        <div className="ccenter w85">
+        <div className="center w85">
             <Header />
             <div className="">
                 <Routes>
@@ -17,6 +18,10 @@ const App = () => {
                     <Route
                         path="/create"
                         element={<CreateLink />}
+                    />
+                    <Route
+                        path="/login"
+                        element={<Login />}
                     />
                 </Routes>
             </div>
