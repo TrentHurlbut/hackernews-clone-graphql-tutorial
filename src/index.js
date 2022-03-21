2f07c1f7b7fa77efaa7344490954bccb9d3aaff8
TrentHurlbut <trent.hurlbut@gmail.com>
2022-03-21 13:40:07 -0700

    Fixed an import.

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import { setContext } from '@apollo/client/link/context';
+import {AUTH_TOKEN} from './constants'
 
 import {
     ApolloProvider,
